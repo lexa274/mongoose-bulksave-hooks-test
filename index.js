048ebc7cb27b5c78a354e5f33570d465ca3d8d7f
Alexandr Tylš <tyls.a@seznam.cz>
2022-10-24 15:16:34 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import mongooseGridFs from 'mongoose-gridfs'
+
+
+async function main () {
+    const mongoDBServer = await MongoMemoryServer.create()
+    await mongoose.connect(mongoDBServer.getUri(), { useUnifiedTopology: true })
+
+    const myModelSchema = new mongoose.Schema({
+        name: String
+    });
+
+    const MyModel = mongoose.model('ModelName', myModelSchema);
+
+    const val1 = new MyModel({ name: 'First' })
+    const val2 = new MyModel({ name: 'Second' })
+    const val3 = new MyModel({ name: 'Third' })
+
+    await MyModel.bulkSave([val1, val2, val3])
+}
+
+main()
